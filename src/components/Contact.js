97b1c5b135fe03837b9e1d98af02f8b11233395d
Melina Boedecker <melinamboedecker@gmail.com>
2021-08-25 22:37:12 -0700

    add contact

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+const Contact = () => {
+    return ( 
+        <div className="contact">
+            {/* <h3>Contact</h3> */}
+            <a href="tel:2098457509">209-845-7509</a> 
+            <br></br>
+            <a href="mailto:melinamboedecker@gmail.com">melinamboedecker@gmail.com</a>
+            
+        </div>
+     );
+}
+ 
+export default Contact;
