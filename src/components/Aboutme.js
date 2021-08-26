0ef5cb4bf9bd108c67b4f8d8520b73a320a73c49
Melina Boedecker <melinamboedecker@gmail.com>
2021-08-25 22:27:23 -0700

    add Footer

---
+++ src/components/Aboutme.js
@@ -0,0 +1,12 @@
+const Aboutme = () => {
+    return ( 
+        <div className="photo-header">
+            <div id="self-photo">
+            <img src={'./assets/bioPhoto.jpg'} alt='bio'/>
+            <p className="about-me-text">Full Stack Web Developer with a background in Analytical Chemistry and Microbiology. </p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Aboutme;
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import { ExternalLink } from 'react-external-link';
+
+export default function Footer() {
+    
+  
+  
+  
+    return (
+  
+   
+        
+            <div className="footer-links">
+                <ExternalLink href="https://github.com/melinamboedecker"><span><i className="fa fa-github"></i></span></ExternalLink>
+                <ExternalLink href="https://www.linkedin.com/in/melina-boedecker-553316200/"><span><i className="fa fa-linkedin-square"></i></span></ExternalLink>
+            </div>
+        
+    
+    );
+  }
